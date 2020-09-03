f410c0babfff7aa6f50fc65b755f01b79c55c5a3
Hoodiiiiii <bkkinfo3@gmail.com>
2020-09-03 10:31:13 +0530

    Update pkb.java

--- src/test/java/testcases/mytheresa/pkb.java
+++ src/test/java/testcases/mytheresa/pkb.java
@@ -30,7 +30,7 @@ public void testgooglrsearch() throws MalformedURLException{
 //FirefoxOptions options = new FirefoxOptions();
 //options.addArguments("-headless");
 //WebDriver driver = new RemoteWebDriver(new URL("http://40.113.221.175:4444/wd/hub"),new FirefoxOptions());
-WebDriver driver = new RemoteWebDriver(new URL("http://52.186.156.179:4444/wd/hub"),new FirefoxOptions());
+RemoteWebDriver driver = new RemoteWebDriver(new URL("http://52.186.156.179:4444/wd/hub"),new FirefoxOptions());
 driver.get("http://www.google.com");
 //we expect the title “Google “ should be present 
 //String Expectedtitle = "Google";
