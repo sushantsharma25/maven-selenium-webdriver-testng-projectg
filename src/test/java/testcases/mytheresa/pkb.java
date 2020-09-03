3f970155b20839a3bbedfd5aa8a62915a1f8d011
Hoodiiiiii <bkkinfo3@gmail.com>
2020-09-03 11:34:58 +0530

    Update pkb.java

--- src/test/java/testcases/mytheresa/pkb.java
+++ src/test/java/testcases/mytheresa/pkb.java
@@ -32,7 +32,7 @@ public void testgooglrsearch() throws MalformedURLException{
 //WebDriver driver = new RemoteWebDriver(new URL("http://40.113.221.175:4444/wd/hub"),new FirefoxOptions());
 RemoteWebDriver driver = new RemoteWebDriver(new URL("http://52.186.156.179:4444/wd/hub"),new FirefoxOptions());
 driver.get("http://www.google.com");
-//Thread.sleep(5000)
+driver.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
 System.out.println(driver.getTitle());
 //Assert.assertEquals("Only Testing");
 //Assert.assertEquals("testing","testing");
